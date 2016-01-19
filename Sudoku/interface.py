36d46bf81dfd7420677db5e1a56361c4fa89cd27
Alex Kasapis <Alex Kasapis>
2016-01-19 23:07:39 +0200

    Sudoku Game

---
+++ Sudoku/interface.py
@@ -0,0 +1,122 @@
+import pygame, pygame.font
+import os, sys
+import sudoku
+
+'''
+Graphical User Interface by: amichalo
+Event Handling by: akasapis
+'''
+
+def main():
+        '''Declaration :
+        The border lines are 5 pixels each. Each block is 30x30 and each line is 2 pixel wide.
+        The area containing the NewGame and Reset buttons has 30 pixels height.
+        This numbers are the ones used when we use specific methods as you can see reading
+        through the code.
+         
+        '''
+        pygame.init()
+
+        # Define colors for easier usage
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        GREEN = (0, 153, 0)
+        RED = (193, 0, 0)
+        GRAY = (112, 138, 144)
+        SAND = (240, 240, 230)
+
+        # Create a font
+        fontObj = pygame.font.Font("PTC55F.ttf", 18)
+        fontObj.set_bold(True)
+
+        #The width and height of the game window
+        xAxis = 294
+        yAxis = 324
+
+        gameWindow = pygame.display.set_mode((xAxis,yAxis))
+        pygame.display.set_caption("SudokuGame")
+        gameWindow.fill(SAND)
+
+        # Create the Buttons (NewGame/Reset)
+        newLabel = fontObj.render("New Game", 1, BLACK)
+        resetLabel = fontObj.render("Reset", 1, BLACK)
+        pygame.draw.line(gameWindow, BLACK, (147,1), (147,30), 4)
+        gameWindow.blit(newLabel, (25,5))
+        gameWindow.blit(resetLabel, (190,5))
+
+        # Create the Borders
+        # Horizontal Borders
+        pygame.draw.line(gameWindow, BLACK, (0,1), (yAxis-1,1), 4)
+        pygame.draw.line(gameWindow, BLACK, (0,yAxis-3), (xAxis,yAxis-3), 4)
+        pygame.draw.line(gameWindow, BLACK, (0,30), (yAxis-1,30), 4)
+        # Vertical Borders
+        pygame.draw.line(gameWindow, BLACK, (xAxis-3,1), (xAxis-3,yAxis), 4)
+        pygame.draw.line(gameWindow, BLACK, (1,0), (1,yAxis-1), 4)
+
+        # Block size is 30x30px; line size is 2px
+        # Draw the lines (every 32px starting from 34)
+        for x in range(34, xAxis, 32):
+                pygame.draw.line(gameWindow, BLACK, (x,30), (x,yAxis), 2)
+        for y in range(64, yAxis, 32):
+                pygame.draw.line(gameWindow, BLACK, (0,y), (xAxis,y), 2)
+
+        # Create and add the number labels to the grid
+        startingTable = sudoku.prepareSudokuTable(30) #This array will not change unless the user starts a new game
+        gameTable = [[startingTable[row][col] for col in range(9)] for row in range(9)] #This array stores the info about the current game
+        for row in range(9): #Add the starting numbers to the grid
+                for col in range(9):
+                        if startingTable[row][col] != 0:
+                                label = fontObj.render(str(gameTable[row][col]), 1, BLACK)
+                                gameWindow.blit(label, (col*32+13,row*32+38))
+
+        pygame.display.update()
+
+        gameExit = False
+        rowToChange = colToChange = -1
+
+        while not gameExit:
+            for event in pygame.event.get():
+                #EXIT EVENTS
+                if event.type == pygame.QUIT:
+                    gameExit = True
+                #MOUSE EVENTS
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    #Check if the User clicked on a block to change its number
+                    if event.button == 1 or event.button == 3:
+                        colToChange = (pygame.mouse.get_pos()[0]-5)/32 #Calculate which row and column on the grid did the User click
+                        rowToChange = (pygame.mouse.get_pos()[1]-35)/32
+                        if event.button == 3: #If the User right clicked, remove the number from the grid, by drawing a rect in front of the number
+                            if startingTable[rowToChange][colToChange]==0:
+                                pygame.draw.rect(gameWindow, SAND, (colToChange*32+5, rowToChange*32+35, 28, 28), 0)
+                                rowToChange = colToChange = -1
+                    #Check if the User clicked on the NewGame/Reset Buttons
+                    if rowToChange==-1 and colToChange>=0 and colToChange<=3:
+                        startingTable = sudoku.prepareSudokuTable(30) #Reset the starting table, the game table, and the grid
+                        gameTable = [[startingTable[row][col] for col in range(9)] for row in range(9)]
+                        for row in range(9):
+                            for col in range(9):
+                                    pygame.draw.rect(gameWindow, SAND, (col*32+5, row*32+35, 28, 28), 0)
+                                    if startingTable[row][col] != 0:
+                                            label = fontObj.render(str(gameTable[row][col]), 1, BLACK)
+                                            gameWindow.blit(label, (col*32+13,row*32+38))
+                    elif rowToChange==-1 and colToChange>=5 and colToChange<=8:
+                        gameTable = [[startingTable[row][col] for col in range(9)] for row in range(9)] #Reset only the game table and the numbers added by the player
+                        for row in range(9):
+                            for col in range(9):
+                                    if startingTable[row][col] == 0:
+                                            pygame.draw.rect(gameWindow, SAND, (col*32+5, row*32+35, 28, 28), 0)
+                #KEYBOARD EVENTS
+                elif event.type == pygame.KEYDOWN:
+                    # If the user has clicked on a block to change the number AND this block was empty in the start AND the input is a number
+                    if rowToChange!=-1 and colToChange!=-1 and startingTable[rowToChange][colToChange]==0 and event.unicode.isdigit()==True and event.unicode!="0":
+                        gameTable[rowToChange][colToChange] = event.unicode #Update the gameTable
+                        pygame.draw.rect(gameWindow, SAND, (colToChange*32+5, rowToChange*32+35, 28, 28), 0) #Cover the previous(possibly) number added by the User
+                        label = fontObj.render(str(gameTable[rowToChange][colToChange]), 1, GREEN) #Create and all the new number
+                        gameWindow.blit(label, (colToChange*32+13,rowToChange*32+38))
+                        rowToChange = colToChange = -1
+                pygame.display.update()
+
+        pygame.quit()
+        quit()
+
+if __name__ == '__main__': main()
---
+++ Sudoku/sudoku.py
@@ -0,0 +1,158 @@
+import random
+
+'''
+Algorithm by: amichalo
+Algorithm implementation: akasapis, amichalo
+'''
+
+def shuffleSet(numberSets, j):
+    '''
+    Author: akasapis
+    Copies the 1st, 2nd or 3rd set of 3 numbers from the first row of the numberSets
+    array, swaps twice this small array and returns it.
+    For example if:
+    1 5 3 | 8 2 9 | 7 6 4
+    5 3 1 | 2 9 8 | 6 4 7
+    3 1 5 | 9 8 2 | 4 7 6
+    is the numberSets and j==1, then tempSet==[8, 2, 9], and after the swaps, it could be
+    [2, 9, 8]
+    '''
+    tempSet = [numberSets[0][z+j*3] for z in range(3)]
+    for swapTimes in range(2):
+        index1 = random.randint(0, 2)
+        index2 = random.randint(0, 2)
+        tempCell = tempSet[index1]
+        tempSet[index1] = tempSet[index2]
+        tempSet[index2] = tempCell
+    return tempSet
+
+def checkValidation(numberSets, tempSet, i, j):
+    '''
+    Author: akasapis
+    Gets the numberSets 2D array, the i and j coords to the numberSets and the tempSet and
+    checks whether the tempSet and the numberSet combinations are unique. For example the
+    combinations can be:
+    [2 4 6] but not [2 4 6]
+    [6 2 4]         [6 4 2]
+    '''
+    for y in range(3):
+        for x in range(i):
+            if tempSet[y] == numberSets[x][y+3*j]:
+               return False
+    return True
+
+def getNumberSets():
+    '''
+    Author: akasapis
+    Create the 3x9 array filled with the number sets that will be used to fill
+    the sudoku table. These will be 3 sets of 3 numbers, in 3 unique combinations.
+    Final example can be:
+    1 5 3 | 8 2 9 | 7 6 4
+    5 3 1 | 2 9 8 | 6 4 7
+    3 1 5 | 9 8 2 | 4 7 6
+    '''
+    # Initialize the array, and fill the first row with 1-9
+    numberSets = [[0 for i in range(9)] for i in range(3)]
+    for i in range(9):
+        numberSets[0][i] = i+1
+    # Shuffle the numbers of the first row
+    for i in range(20):
+        index1 = random.randint(0, 8)
+        index2 = random.randint(0, 8)
+        tempCell = numberSets[0][index1]
+        numberSets[0][index1] = numberSets[0][index2]
+        numberSets[0][index2] = tempCell
+    # Create the combinations of the sets
+    for i in range(2): # Which row
+        for j in range(3): # Which combination
+            tempSet = shuffleSet(numberSets, j)
+            validSet = False
+            while validSet == False:
+                validSet = checkValidation(numberSets, tempSet, i+1, j)
+                if validSet == False:
+                    tempSet = shuffleSet(numberSets, j)
+            numberSets[i+1][0+3*j] = tempSet[0]
+            numberSets[i+1][1+3*j] = tempSet[1]
+            numberSets[i+1][2+3*j] = tempSet[2]
+    return numberSets
+
+def getSets(numberSets, setsNumber):
+    '''
+    Author: akasapis
+    Gets the numberSets 2D array and which one of the 3 sets of combinations the
+    program wants. For example, if:
+    1 5 3 | 8 2 9 | 7 6 4
+    5 3 1 | 2 9 8 | 6 4 7
+    3 1 5 | 9 8 2 | 4 7 6
+    is the numberSets and the setsNumber is equal to 1, the return value will be:
+    8 2 9
+    2 9 8
+    9 8 2
+    '''
+    tempSets = [[0 for i in range(3)] for i in range(3)]
+    for i in range(3):
+        for j in range(3):
+            tempSets[i][j] = numberSets[i][j+3*setsNumber]
+    return tempSets
+
+def getEmptyRow(sudokuTable, bigRow):
+    '''
+    Author: akasapis
+    Gets the main sudoku table, which is being filled, and one of the three main rows
+    (big block) and returns a random row of that block the is not yet filled with numbers.
+    '''
+    randRow = random.randint(0, 2)
+    while True:
+        if sudokuTable[randRow+bigRow*3][0] == 0:
+            return randRow
+        randRow = (randRow + 1) % 3
+
+def fillTable(sudokuTable, numberSets):
+    '''
+    Author: akasapis
+    Fills the sudoku table with the cycle algorithm.
+    '''
+    for setsNumber in range(3):
+        tempSets = getSets(numberSets, setsNumber)
+        for bigRow in range(3):
+            startingRow = getEmptyRow(sudokuTable, bigRow)
+            for bigCol in range(3):
+                sudokuTable[startingRow+bigRow*3][0+bigCol*3] = tempSets[(bigRow+bigCol)%3][0]
+                sudokuTable[startingRow+bigRow*3][1+bigCol*3] = tempSets[(bigRow+bigCol)%3][1]
+                sudokuTable[startingRow+bigRow*3][2+bigCol*3] = tempSets[(bigRow+bigCol)%3][2]
+                startingRow = (startingRow + 1) % 3
+    return sudokuTable
+
+def createFullTable():
+    '''
+    Author: akasapis
+    Create the numberSets and initialize the sudoku table, and then call the fillTable method
+    to fill the table with the sets.
+    '''
+    numberSets = getNumberSets()
+    sudokuTable = [[0 for i in range(9)] for i in range(9)]
+    sudokuTable = fillTable(sudokuTable, numberSets)
+    return sudokuTable
+
+def prepareSudokuTable(number):
+    '''
+    Author: amichalo
+    Get the full table with the createFullTable function and then randomly copy numbers from
+    the full table to the empty one.
+    '''
+    fullTable = createFullTable()
+    sudokuTable = [[0 for i in range(9)] for i in range(9)]
+    for i in range(number):
+        row = random.randint(0, 8)
+        column = random.randint(0, 8)
+        if sudokuTable[row][column] == 0:
+            sudokuTable[row][column] = fullTable[row][column]
+        else:
+            while sudokuTable[row][column] != 0:
+                 row = random.randint(0, 8)
+                 column = random.randint(0, 8)
+                 if sudokuTable[row][column] == 0:
+                     sudokuTable[row][column] = fullTable[row][column]
+                     break
+    return sudokuTable
+
